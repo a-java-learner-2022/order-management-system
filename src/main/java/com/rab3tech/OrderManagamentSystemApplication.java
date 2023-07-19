9ed837bbd12c6725356d7776f5554110a7083b2f
a-java-learner-2022 <a.java.learner.2022@gmail.com>
2023-07-19 19:25:21 -0400

    day1: project setup, dependencies added.

---
+++ src/main/java/com/rab3tech/OrderManagamentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rab3tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagamentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagamentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rab3tech/ordermanagamentsystem/OrderManagamentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rab3tech.ordermanagamentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagamentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
