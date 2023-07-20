104be3139268666af677d6678188d2f0a36c5f20
a-java-learner-2022 <a.java.learner.2022@gmail.com>
2023-07-20 16:09:57 -0400

    day 2 : application.properties configured for database + jpa properties, Model Customer added.

---
+++ src/main/java/com/rab3tech/model/Customer.java
@@ -0,0 +1,37 @@
+package com.rab3tech.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="customer_tbl")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data // for getters and setters
+public class Customer {
+	@Id
+	@Column(name="customer_id", nullable=false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long customerId;
+	@Column(name="firstname", nullable=false)
+	private String firstname;
+	@Column(name="lastname", nullable=false)
+	private String lastname;
+	@Column(name="address", nullable=false)
+	private String address;
+	@Column(name="mobile", nullable=false)
+	private String mobile;
+	@Column(name="email", nullable=false)
+	private String email;
+	@Column(name="password", nullable=false)
+	private String password;
+
+}
